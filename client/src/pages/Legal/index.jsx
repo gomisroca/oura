ad3e39985836beccc5197918205ec7442ad2248c
asfalfa <glkin9@gmail.com>
2024-02-29 14:55:21 +0100

    feat(Client): :construction: Placeholder legal page

---
+++ client/src/pages/Legal/index.jsx
@@ -0,0 +1,37 @@
+
+
+function Legal() {
+  return (
+    <div className='flex flex-col overflow-hidden h-full'>
+        <div className="w-2/3 mx-auto px-4 mt-2 text-justify">
+            <div className="text-xl">
+            Lorem ipsum
+            </div>
+            <div className="mx-1 text-black/70">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices quis tortor ut placerat. Maecenas congue, justo vel molestie pharetra, leo erat bibendum augue, ut convallis sem neque eu ipsum. Donec leo mi, pharetra a mi eget, imperdiet consequat purus. Quisque nec ipsum ac erat luctus suscipit. Nunc pharetra ullamcorper arcu, at consectetur ante laoreet quis. Sed aliquam ultrices mi ac facilisis. Maecenas elit enim, semper a mollis eget, dapibus consequat purus. Aenean dapibus eros non ante rhoncus gravida. Nullam ultricies iaculis dui, sed rhoncus turpis euismod eu. 
+            </div>
+            <div className="text-xl">
+            Lorem ipsum
+            </div>
+            <div className="mx-1 text-black/70">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices quis tortor ut placerat. Maecenas congue, justo vel molestie pharetra, leo erat bibendum augue, ut convallis sem neque eu ipsum. Donec leo mi, pharetra a mi eget, imperdiet consequat purus. Quisque nec ipsum ac erat luctus suscipit. Nunc pharetra ullamcorper arcu, at consectetur ante laoreet quis. Sed aliquam ultrices mi ac facilisis. Maecenas elit enim, semper a mollis eget, dapibus consequat purus. Aenean dapibus eros non ante rhoncus gravida. Nullam ultricies iaculis dui, sed rhoncus turpis euismod eu. 
+            </div>
+            <div className="text-xl">
+            Lorem ipsum
+            </div>
+            <div className="mx-1 text-black/70">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices quis tortor ut placerat. Maecenas congue, justo vel molestie pharetra, leo erat bibendum augue, ut convallis sem neque eu ipsum. Donec leo mi, pharetra a mi eget, imperdiet consequat purus. Quisque nec ipsum ac erat luctus suscipit. Nunc pharetra ullamcorper arcu, at consectetur ante laoreet quis. Sed aliquam ultrices mi ac facilisis. Maecenas elit enim, semper a mollis eget, dapibus consequat purus. Aenean dapibus eros non ante rhoncus gravida. Nullam ultricies iaculis dui, sed rhoncus turpis euismod eu. 
+            Integer rhoncus tempor dolor, in commodo nisl aliquam a. Praesent non commodo tortor. Nam leo arcu, finibus nec nulla quis, sagittis aliquam elit. Phasellus blandit tincidunt ligula eu euismod. Duis tincidunt auctor feugiat. Cras aliquet libero augue, tempor mollis lacus iaculis sed. Aliquam vel auctor ex.
+
+Aliquam eros leo, ultrices vitae cursus et, tristique ut eros. Nullam sagittis mauris a placerat porttitor. Nullam commodo mauris risus, laoreet auctor purus ullamcorper non. Ut feugiat ac lacus vel tempor. Sed sagittis dui a maximus ornare. Vivamus aliquet mi ut justo pretium, nec malesuada nisi sagittis. Fusce ligula elit, maximus feugiat sagittis in, placerat laoreet diam. Praesent ac neque vel dui tincidunt facilisis et non nulla.
+
+Integer quis sapien ac mauris faucibus auctor vel ac purus. Aenean vulputate tortor non convallis fermentum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean non nibh egestas, commodo leo vitae, tristique tellus. Nunc nec lacus quam. Etiam fermentum velit augue, in porttitor elit porttitor eu. Curabitur odio nisi, faucibus non tempor bibendum, molestie nec metus. Proin consequat pharetra sapien sit amet volutpat. Aliquam ac molestie erat. Aenean gravida, dui vitae maximus malesuada, mauris diam ultricies justo, ut rhoncus mi ante dignissim mauris. Morbi consequat mauris vel mi vulputate, non fringilla ante posuere. Cras pretium, nisi ac cursus congue, ligula tellus aliquet dolor, in egestas neque nulla ac dui. Praesent ut velit dui. Quisque sodales, dui eget pulvinar vestibulum, libero sem auctor risus, id fermentum sapien quam vitae tortor. Donec vitae tincidunt purus, at tincidunt sem. Morbi suscipit ut dui a sodales.
+
+Aliquam sit amet nisi ac ipsum finibus imperdiet sed non arcu. Sed ut accumsan tellus, ac iaculis justo. Donec sed augue ex. Cras scelerisque ultricies semper. Phasellus commodo egestas risus non luctus. Nunc non porttitor dui. Vivamus at sem purus. Nam convallis interdum finibus. Curabitur et dui cursus, faucibus elit et, sodales sapien. Nulla id dolor vitae turpis porta sollicitudin. Aenean vehicula velit nec sem tempus egestas. Morbi scelerisque gravida massa. Vivamus nec elit vulputate, suscipit lorem vel, commodo nunc. Quisque fermentum dapibus lacus sit amet euismod. 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Legal
