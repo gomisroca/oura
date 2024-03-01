671a22f42a9d1ea4877cf73ff3a586fb0860bd99
asfalfa <glkin9@gmail.com>
2024-03-01 22:24:42 +0100

    feat(API): :sparkles: Implemented JWT verification and decoupled fetching user info from login endpoint

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const Order = require('../models/order')
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const verifyToken = require('../middleware/auth');
 
 router.post("/register", async (req, res) => {
     try {
@@ -29,16 +31,12 @@ router.post("/register", async (req, res) => {
             password: encryptedPassword,
         });
         
-        const token = jwt.sign(
-            { user_id: user._id, email },
-            process.env.TOKEN_KEY,
-            {
-            expiresIn: "2h",
-            }
+        let access_token = jwt.sign(
+            { user_id: user._id, email, first_name: user.first_name, last_name: user.last_name },
+            process.env.TOKEN_KEY
         );
-        user.token = token;
     
-        res.status(201).json(user);
+        res.status(201).json(access_token);
     } catch (err) {
         console.log(err);
     }
@@ -54,31 +52,44 @@ router.post("/login", async (req, res) => {
             const user = await User.findOne({ email });
 
             if (user && (await bcrypt.compare(password, user.password))) {
-                // Create token
-                let token;
+                let access_token;
                 if (keepAlive == true) {
-                    token = jwt.sign(
-                        { user_id: user._id, email },
+                    access_token = jwt.sign(
+                        { user_id: user._id, email, first_name: user.first_name, last_name: user.last_name },
                         process.env.TOKEN_KEY
                     );
                     
                 } else {
-                    token = jwt.sign(
-                        { user_id: user._id, email },
+                    access_token = jwt.sign(
+                        { user_id: user._id, email, first_name: user.first_name, last_name: user.last_name },
                         process.env.TOKEN_KEY,
                         {
-                            expiresIn: "2h",
+                            expiresIn: "1h",
                         }
                     );
                 }
-                // save user token
-                user.token = token;
-
-                res.status(200).json(user);
+                
+                res.status(200).json(access_token);
             } else {
                 res.status(400).send("Invalid Credentials");
             }
         }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/info", verifyToken, async (req, res) => {
+    res.status(200).json(req.user);
+});
+
+router.post("/purchase", verifyToken, async (req, res) => {
+    try{        
+        const order = await Order.create({
+           user: "1",
+           products: ["1","24235", "12489124-1248124bcew-1293neh"]
+        });
+        console.log(order)
     } catch (err) {
         console.log(err);
     }
