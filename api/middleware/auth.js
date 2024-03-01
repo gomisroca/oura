c50852ede1049226877d7b03da35d332e2c2ee88
asfalfa <glkin9@gmail.com>
2024-03-01 22:21:56 +0100

    fix(API): :bug: token middleware looks for token in auth header, next only if the token is verified

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,19 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
-
+  const token = req.headers.authorization;
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    return res.status(400).send("A token is required for authentication");
+  } else {
+    try {
+      const decodedUserData = jwt.verify(token, process.env.TOKEN_KEY);
+      req.user = decodedUserData;
+      return next();
+    } catch (err) {
+      return res.status(401).send("Invalid Token");
+    }
   }
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
 };
 
 module.exports = verifyToken;
