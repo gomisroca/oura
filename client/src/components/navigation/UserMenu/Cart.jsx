75668da86bf845b79fa1f0502992261b297fa979
asfalfa <glkin9@gmail.com>
2024-03-02 19:07:48 +0100

    style(Client): :lipstick: Improved cart styling for X button

--- client/src/components/navigation/UserMenu/Cart.jsx
+++ client/src/components/navigation/UserMenu/Cart.jsx
@@ -13,13 +13,14 @@ export default function Cart() {
     const [cartChanged, setChange] = React.useState(false)
     const [cartEl, setCartEl] = React.useState(null);
     const openCart = Boolean(cartEl);
-
+    
     let cart = localStorage.getItem('oura_cart');
     if(cart == null){
-      cart = [];
+        cart = [];
     } else{
-      cart = JSON.parse(cart);
+        cart = JSON.parse(cart);
     }
+
     let totalCartPrice = 0;
     for (let i = 0; i < cart.length; i++) {
         if(cart[i].sale){
@@ -49,76 +50,85 @@ export default function Cart() {
     return (
         <>
             <IconButton
-                className='hover:stroke-zinc-400'
-                id="cart-button"
-                aria-controls={openCart ? 'cart-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={openCart ? 'true' : undefined}
-                onClick={handleCart}
-                aria-label={`Cart`}
-            >
+            className='hover:stroke-zinc-400'
+            id="cart-button"
+            aria-controls={openCart ? 'cart-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={openCart ? 'true' : undefined}
+            onClick={handleCart}
+            aria-label={`Cart`}>
                 <ShoppingCartTwoToneIcon />
             </IconButton>
 
             <Menu
-                id="cart-menu"
-                anchorEl={cartEl}
-                open={openCart}
-                onClose={handleCartClose}
-                MenuListProps={{
-                'aria-labelledby': 'cart-button',
-                sx: { py: 0 }
-                }}
-                PaperProps={{
-                sx: { borderRadius: 0 }
-                }}
-            >   
+            id="cart-menu"
+            anchorEl={cartEl}
+            open={openCart}
+            onClose={handleCartClose}
+            MenuListProps={{
+            'aria-labelledby': 'cart-button',
+            sx: { py: 0 }
+            }}
+            PaperProps={{
+            sx: { borderRadius: 0 }
+            }}>   
                 <div className='bg-zinc-200 text-zinc-700'>
                     {cart[0] == null ? 
                         <MenuItem className='w-screen md:w-[500px] cursor-default'>
-                            <ListItemText className='p-5 cursor-default text-center'>Nothing in your cart yet</ListItemText>
+                            <ListItemText className='p-5 cursor-default text-center'>
+                                Nothing in your cart yet
+                            </ListItemText>
                         </MenuItem> 
-                    : ''}
+                    : null}
                     {cart.map((item) => (
                     <div
-                    className='w-screen md:w-[500px] flex flex-row text-start items-center'
+                    className='w-screen md:w-[500px] h-[120px] flex flex-row text-start items-center'
                     key={item.cartID}>
                         <div 
-                        className='p-2 w-full flex flex-row hover:bg-zinc-300 items-center'
-                        onClick={() => navigate('/' + item.genre + '/' + item.class + '/' + item.type + '/' + item.id)}
-                        >
+                        className='p-2 w-full flex flex-row hover:bg-zinc-300 items-center cursor-pointer'
+                        onClick={() => navigate('/' + item.genre + '/' + item.class + '/' + item.type + '/' + item.id)}>
                             <div className='w-[50px]'>
                                 <img
                                 className='max-w-[50px] max-h-[50px]'
                                 src={`${item.image}`}
                                 srcSet={`${item.image}`}
                                 alt={item.title}
-                                loading="lazy"
-                                />
+                                loading="lazy" />
                             </div>
                             <ListItemText className='text-left self-center px-5'>
-                                <span>{item.title}</span>
+                                <span className='font-semibold'>{item.title}</span>
                                 <br />
                                 {item.chosenSize ? 
                                     <div>
-                                    <span className='text-sm'>Size: <span className='font-bold'>{item.chosenSize.toUpperCase()}</span></span>
+                                    <span className='text-sm'>
+                                        Size: <span className='font-bold'>{item.chosenSize.toUpperCase()}</span>
+                                    </span>
                                     <br />
-                                    <span className='text-sm'>Color: <span className='font-bold'>{item.chosenColor.toUpperCase()}</span></span>
+                                    <span className='text-sm'>
+                                        Color: <span className='font-bold'>{item.chosenColor.toUpperCase()}</span>
+                                    </span>
                                     <br />
                                     </div>
-                                    : undefined
-                                }
-                                <span className='font-bold'>{item.sale ? item.sale : item.price }€</span>
+                                : null}
+                                <span className='font-bold'>
+                                    {item.sale ? item.sale : item.price }€
+                                </span>
                             </ListItemText>
                         </div>
-                        <ListItemIcon onClick={() => handleItemRemove(item.cartID)}>
-                            <ClearIcon className='cursor-pointer hover:bg-zinc-300' />
-                        </ListItemIcon>
+                        <div 
+                        className='flex cursor-pointer hover:bg-zinc-300 h-full items-center justify-items-center'
+                        onClick={() => handleItemRemove(item.cartID)}>
+                            <ListItemIcon>
+                                <ClearIcon className='m-auto' />
+                            </ListItemIcon>
+                        </div>
                     </div>
                     ))}
                     {cart[0] != null ? 
                     <div className='sticky bottom-0 bg-zinc-200 border-t-2 border-zinc-400  flex justify-end p-3 items-center cursor-default'>
-                        <span className='mx-5 text-sm'>TOTAL <span className='text-lg'>{totalCartPrice}€</span></span>
+                        <span className='mx-5 text-sm'>
+                            TOTAL <span className='text-lg'>{totalCartPrice}€</span>
+                        </span>
                         <div onClick={() => navigate("/checkout")}>
                             <button 
                             className='font-semibold border-2 p-2 border-zinc-400 hover:border-zinc-500 rounded hover:bg-gradient-to-br hover:from-zinc-200 hover:to-zinc-300' 
@@ -127,10 +137,9 @@ export default function Cart() {
                             </button>
                         </div>
                     </div> 
-                    : '' }
+                    : null}
                 </div>
             </Menu>
-
         </>
     )
 }
