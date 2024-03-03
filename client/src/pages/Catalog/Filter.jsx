9f69cbc3f714e243a513b15fa62812c6e1d49d13
asfalfa <glkin9@gmail.com>
2024-03-03 17:19:12 +0100

    feat(Client): :sparkles: Moved catalog filter to its own component and adjusted styling for consistency

--- client/src/pages/Catalog/Filter.jsx
+++ client/src/pages/Catalog/Filter.jsx
@@ -1,30 +1,65 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-const Filter = () => {  
-    const [sizeFilter, setSizeFilter] = React.useState('');
-    const [typeFilter, setTypeFilter] = React.useState('');
+export default function Filter({ products, updateProducts }) {
+    const sizes = ['XS','S','M','L','XL','XXL'];
+    const [sizeFilter, setSizeFilter] = useState(null);
+
+    const filterProducts = () => {
+        if(sizeFilter == null || sizeFilter == ''){
+            updateProducts(products);
+        }
+        else{
+            let filteredProducts = products.filter(item => {
+                if (sizeFilter && item.sizes) {
+                    for(let i = 0; item.sizes[sizeFilter].length; i++){
+                        if(item.sizes[sizeFilter][i]){
+                        if(item.sizes[sizeFilter][i].amount > 0){
+                            return true;
+                        }
+                        } else{
+                        return false;
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            });
+
+            updateProducts(filteredProducts);
+        }
+    }
+
+    useEffect(() => {
+        if(products){
+            filterProducts();
+        }
+    }, [sizeFilter])
+
+    useEffect(() => {
+        setSizeFilter('')
+    }, [products])
+   
 
     return (
-        <div>
-            <select value={sizeFilter} onChange={e => setSizeFilter(e.target.value)}>
-                <option value="">All sizes</option>
-                <option value="XS">XS</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-                <option value="XL">XL</option>
-            </select>
-
-            <select value={typeFilter} onChange={e => setTypeFilter(e.target.value)}>
-                <option value="">All types</option>
-                <option value="T-shirt">T-shirt</option>
-                <option value="Jeans">Jeans</option>
-                <option value="Jacket">Jacket</option>
-                <option value="Sweater">Sweater</option>
-                <option value="Dress">Dress</option>
+        <div className='bg-zinc-200 w-fit border-b border-zinc-400'>
+            <span className='border-r border-zinc-400 px-4 py-2 cursor-default'>Size</span>
+            <select
+            className='bg-zinc-200 hover:bg-zinc-300 px-4 py-2 cursor-pointer font-semibold'
+            value={sizeFilter}
+            onChange={e => setSizeFilter(e.target.value)}>
+                <option
+                value=''>
+                    All
+                </option>
+                <hr />
+                {sizes.map((size) => (
+                    <option
+                    key={size}
+                    value={size}>
+                        {size}
+                    </option>
+                ))}
             </select>
         </div>
     );
 };
-
-export default Filter;
--- client/src/pages/Catalog/index.jsx
+++ client/src/pages/Catalog/index.jsx
@@ -1,162 +1,164 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, useOutletContext } from "react-router-dom";
+import axios from 'axios';
+
+import Skeleton from '@mui/material/Skeleton';
+
 import ManImg from '../../assets/categories/man.jpg';
 import WomanImg from '../../assets/categories/woman.jpg';
 import OutdoorsImg from '../../assets/categories/outdoors.jpg';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Filter from './Filter';
-import { useNavigate, useParams, useOutletContext } from "react-router-dom";
-import axios from 'axios';
-import React from "react";
-import Skeleton from '@mui/material/Skeleton';
 
-function Catalog() {
+import Filter from './Filter';
+
+export default function Catalog() {
     const navigate = useNavigate();
-    let genre = useParams().genre;
-    let category = useParams().category;
-    let type = useParams().type;
+
+    const genre = useParams().genre;
+    const category = useParams().category;
+    const type = useParams().type;
 
     const isMobile = useOutletContext();
 
-    const [catalog, setCatalog] = React.useState([]);
-    React.useEffect(() => {
+    const [catalog, setCatalog] = useState(null);
+    const [products, setProducts] = useState(null);
+    const [sizeFilteredProduct, setSizeFilteredProducts] = useState(null);
+
+    const fetchCatalog = () => {
         axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/clothes/catalog`)
         .then((res) => {
-        setCatalog(res.data);
+            setCatalog(res.data);
+            filterCatalog(res.data);
         })
         .catch(error => {
-        if(error.response){
-            console.log(error.response)
-        } else if(error.request){
-            console.log(error.request)
-        } else{
-            console.log(error.message)
-        }
+            if(error.response){
+                console.log(error.response)
+            } else if(error.request){
+                console.log(error.request)
+            } else{
+                console.log(error.message)
+            }
         })
-    }, []);
-
-    let products = catalog.filter(x => (x.genre.toLowerCase() == genre || x.genre == 'neutral'));
-    if(category == 'season'){
-        products = products.filter(x => x.seasonal == true);
-    } else if (category != undefined) {
-        products = products.filter(x => x.class == category);
     }
-    if(type != undefined){
-        products = products.filter(x => x.type == type);
+    
+    const filterCatalog = (catalog) => {
+        let productArray = catalog.filter(x => (x.genre.toLowerCase() == genre || x.genre == 'neutral'));
+        if(category == 'season'){
+            productArray = productArray.filter(x => x.seasonal == true);
+        } else if (category != undefined) {
+            productArray = productArray.filter(x => x.class == category);
+        }
+        if(type != undefined){
+            productArray = productArray.filter(x => x.type == type);
+        }
+        setProducts(productArray);
+        setSizeFilteredProducts(productArray);
+    }
+
+    useEffect(() => {
+        if (catalog) {
+            filterCatalog(catalog);
+        } else{
+            fetchCatalog();
+        }
+    }, [genre, category, type])
+
+    const handleUpdateProducts = (data) => {
+        setSizeFilteredProducts(data);
     }
 
     let banner;
     switch(genre){
         case 'man':
-        banner = ManImg;
-        break;
+            banner = ManImg;
+            break;
         case 'woman':
-        banner = WomanImg;
-        break;
+            banner = WomanImg;
+            break;
         default:
-        banner = OutdoorsImg;
-        break;
+            banner = OutdoorsImg;
+            break;
     }
-    
-    //Filter Handling
-    const sizes = ['XS','S','M','L','XL','XXL'];
-    const [sizeFilter, setSizeFilter] = React.useState(null);
-
-    const colors = ['black','white','red','orange','yellow','blue','purple', 'green'];
-    const [colorFilter, setColorFilter] = React.useState(null);
-
-    const filteredClothes = products.filter(item => {
-        if(sizeFilter == null || sizeFilter == 'All sizes'){
-        return true;
-        }
-        if (sizeFilter && item.sizes) {
-        for(let i = 0; item.sizes[sizeFilter].length; i++){
-            if(item.sizes[sizeFilter][i]){
-            if(item.sizes[sizeFilter][i].amount > 0){
-                return true;
-            }
-            } else{
-            return false;
-            }
-        }
-        } else {
-        return false;
-        }
-    });
 
     return (
         <div className='flex flex-col overflow-hidden h-full text-zinc-700'>
-        <div className='grid h-[100px] md:h-[400px] w-screen'>
-            {banner ? 
-            <img
-            className= 'h-full w-screen brightness-75'
-            src={banner}
-            alt="Sale Image"
-            />
-            : <Skeleton variant="rectangular" />}
-            {category ? 
-            <div className='cursor-default absolute uppercase text-[20px] md:text-[50px] text-zinc-200 self-center justify-self-center mb-[50px] md:mb-[180px]'>
-            {genre}
-            </div> 
-            : 
-            <div className='cursor-default absolute uppercase text-[50px] md:text-[200px] text-zinc-200 self-center justify-self-center'>
-            {genre}
-            </div>}
-            {category ? 
-            <div className='cursor-default absolute uppercase text-[50px] md:text-[200px] text-zinc-200 self-center justify-self-center'>
-            {category}
-            </div> 
-            : undefined}
-            {type ? 
-            <div className='absolute uppercase text-[35px] md:text-[100px] text-zinc-200 self-center justify-self-center mt-[65px] md:mt-[230px]'>
-            {type}
-            </div> 
-            : undefined }
-        </div>
-        
-        <div>
-            <select value={sizeFilter} onChange={e => setSizeFilter(e.target.value)}>
-            <option>All sizes</option>
-            {sizes.map((size) => (
-                <option key={size} value={size}>{size}</option>
-            ))}
-            </select>
-        </div>
+            <div className='grid h-[100px] md:h-[400px] w-screen'>
+                {banner ? 
+                <img
+                className= 'w-screen brightness-75'
+                src={banner}
+                alt="Sale Image"
+                />
+                : 
+                <Skeleton variant="rectangular" />
+                }
 
-        {products ?
-            <div className='mx-auto p-5 mb-5 text-zinc-700'>
-            <ImageList cols={isMobile ? 2 : 6} gap={2}>
-                {filteredClothes.sort(({sales:a}, {sales:b}) => b-a).map((item) => (
-                <div 
-                key={item.id}
-                className='h-fit flex relative flex-col' 
-                onClick={() => navigate('/' + item.genre + '/' + item.class + '/' + item.type + '/' + item.id)}>
-                    <ImageListItem  
-                    className='transition duration-200 bg-zinc-200 hover:bg-zinc-300 border-zinc-400 hover:border-zinc-500 hover:text-zinc-800 border-2 w-[150px] md:w-[200px]' 
-                    >
-                    <img
-                        className='max-h-[200px] md:max-h-[250px]'
-                        src={`${item.image}`}
-                        srcSet={`${item.image}`}
-                        alt={item.title}
-                        loading="lazy"
-                    />
-                    <ImageListItemBar
-                        className='mx-2'
-                        position="below"
-                        title={item.title}
-                        subtitle={item.sale ? <span>{item.sale}€ <span className="line-through decoration-2 decoration-red-600/70">{item.price}€</span></span> : item.price + '€' }
-                    />
-                    </ImageListItem>
+                {category ? 
+                <div className='cursor-default absolute uppercase text-[20px] md:text-[50px] text-zinc-200 self-center justify-self-center mb-[50px] md:mb-[180px]'>
+                    {genre}
+                </div> 
+                : 
+                <div className='cursor-default absolute uppercase text-[50px] md:text-[200px] text-zinc-200 self-center justify-self-center'>
+                    {genre}
                 </div>
-                ))}
-            </ImageList>
+                }
+
+                {category ? 
+                <div className='cursor-default absolute uppercase text-[50px] md:text-[200px] text-zinc-200 self-center justify-self-center'>
+                    {category}
+                </div> 
+                : 
+                undefined
+                }
+
+                {type ? 
+                <div className='absolute uppercase text-[35px] md:text-[100px] text-zinc-200 self-center justify-self-center mt-[65px] md:mt-[230px]'>
+                    {type}
+                </div> 
+                : 
+                undefined
+                }
             </div>
-        : 
-            undefined 
-        }
+            
+            <Filter products={products} updateProducts={handleUpdateProducts} />
+
+            {sizeFilteredProduct ?
+            <div className='mx-auto p-5 mb-5 '>
+                <div className="grid grid-cols-3 md:grid-cols-5 gap-2">
+                    {sizeFilteredProduct.sort(({sales:a}, {sales:b}) => b-a).map((item) => (
+                    <div 
+                    key={item.id}
+                    className='cursor-pointer transition duration-200 h-[275px] md:h-[350px] w-[175px] md:w-[225px] flex relative flex-col bg-zinc-200 hover:bg-zinc-300 border-zinc-400 hover:border-zinc-500 text-zinc-700 hover:text-zinc-800 border-2' 
+                    onClick={() => navigate('/' + item.genre + '/' + item.class + '/' + item.type + '/' + item.id)}>
+                        <div className="h-2/3 md:h-3/4 overflow-y-hidden">
+                            <img
+                            src={`${item.image}`}
+                            srcSet={`${item.image}`}
+                            alt={item.title}
+                            loading="lazy"
+                            />
+                        </div>
+                        <div
+                        className='mx-2 py-2 flex flex-col absolute bottom-[2px]'>
+                            <span className="font-semibold">{item.title}</span>
+                            {item.sale ?
+                            <div className="flex flex-row gap-x-2">
+                                <span>
+                                    {item.sale}€
+                                </span> 
+                                <span className="line-through decoration-2 decoration-red-600/70">
+                                    {item.price}€
+                                </span>
+                            </div>
+                            : 
+                            <span>{item.price}€</span>
+                            }
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            </div>
+            : 
+            null}
         </div>
     )
 }
-
-export default Catalog
