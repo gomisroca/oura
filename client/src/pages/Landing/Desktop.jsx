b5a673ad04759f77f3338653e6efd0a5aa0c13d3
asfalfa <glkin9@gmail.com>
2024-03-03 21:36:07 +0100

    chore(Client): :speech_balloon: Updated some texts

--- client/src/pages/Landing/Desktop.jsx
+++ client/src/pages/Landing/Desktop.jsx
@@ -12,9 +12,15 @@ function DesktopLayout() {
             src={LandingImg}
             alt="OURA Landing"
             />
-            <div className='absolute bottom-2/4 right-4 text-center w-full text-[4rem] md:text-[5rem] 2xl:text-[10rem] font-semibold opacity-30'>OURA</div>
-            <div className='absolute bottom-2/4 right-2 text-center w-full text-[4rem] md:text-[5rem] 2xl:text-[10rem] font-semibold opacity-60'>OURA</div>
-            <div className='absolute bottom-2/4 text-center w-full text-[4rem] md:text-[5rem] 2xl:text-[10rem] font-semibold'>OURA</div>
+            <div className='absolute bottom-2/4 right-4 text-center w-full text-[4rem] md:text-[5rem] 2xl:text-[10rem] font-semibold opacity-30'>
+                OURA
+            </div>
+            <div className='absolute bottom-2/4 right-2 text-center w-full text-[4rem] md:text-[5rem] 2xl:text-[10rem] font-semibold opacity-60'>
+                OURA
+            </div>
+            <div className='absolute bottom-2/4 text-center w-full text-[4rem] md:text-[5rem] 2xl:text-[10rem] font-semibold'>
+                OURA
+            </div>
             <div className='absolute self-center mt-16 w-full grid justify-items-center'>
                 <span className='px-2 py-1 md:px-5 md:py-2 text-[1rem] md:text-[3rem] font-semibold'>
                     SPRING COLLECTION
