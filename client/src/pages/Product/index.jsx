15e4c001638eddc300628f7e4dcdcc6dabe39e9f
asfalfa <glkin9@gmail.com>
2024-03-05 16:27:51 +0100

    feat(Client): :sparkles: Reworked product styling and moved related items to its own component

--- client/src/pages/Product/index.jsx
+++ client/src/pages/Product/index.jsx
@@ -1,32 +1,25 @@
-import { useParams, useOutletContext, useNavigate } from "react-router-dom";
-import IconButton from '@mui/material/IconButton';
-import AlarmIcon from '@mui/icons-material/Alarm';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Tooltip from '@mui/material/Tooltip';
-import Fade from '@mui/material/Fade';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from 'axios';
-import React from "react";
+
 import Skeleton from '@mui/material/Skeleton';
-import Sizes from "./Sizes";
+
+import SizeMenu from "./SizeMenu";
 import AddToCart from "./AddToCart";
+import RelatedItems from "./RelatedItems";
 
 function Product() {
-    const navigate = useNavigate();
-
     const genre = useParams().genre;
     const category = useParams().category;
-    const type = useParams().type;
     const id = useParams().product;
-    const isMobile = useOutletContext();
 
-    const [product, setProduct] = React.useState([]);
-    React.useEffect(() =>{
-        if(id){
-        axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/clothes/item/${id}`)
+    const [product, setProduct] = useState();
+    const [catalog, setCatalog] = useState();
+
+    const fetchCatalog = () => {
+        axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/clothes/catalog`)
         .then((res) => {
-            setProduct(res.data);
+            setCatalog(res.data);
         })
         .catch(error => {
             if(error.response){
@@ -37,112 +30,73 @@ function Product() {
                 console.log(error.message)
             }
         })
+    }
+
+    useEffect(() => {
+        fetchCatalog();
+    }, []);
+
+    useEffect(() => {
+        if(id){
+            axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/clothes/item/${id}`)
+            .then((res) => {
+                setProduct(res.data);
+            })
+            .catch(error => {
+                if(error.response){
+                    console.log(error.response)
+                } else if(error.request){
+                    console.log(error.request)
+                } else{
+                    console.log(error.message)
+                }
+            })
         }
     }, [id]);
-    let item = product;
-
-    const [catalog, setCatalog] = React.useState([]);
-    React.useEffect(() => {
-        axios.get(`${import.meta.env.VITE_REACT_APP_API_URL}/clothes/catalog`)
-        .then((res) => {
-        setCatalog(res.data);
-        })
-        .catch(error => {
-        if(error.response){
-            console.log(error.response)
-        } else if(error.request){
-            console.log(error.request)
-        } else{
-            console.log(error.message)
-        }
-        })
-    }, []);
-    let itemData = catalog.filter(x => (x.genre == genre || x.genre == 'neutral') && x.class == category);
-    let filter = itemData.find(x => x.id == item.id);
-    const index = itemData.indexOf(filter);
-    if (index > -1) {
-        itemData.splice(index, 1);
-    }
-    itemData = itemData.slice(0,4);
 
     return (
-        <div className='flex flex-col overflow-hidden text-gray-700'>
-        {item ?
-        <div className="p-10 cursor-default w-screen md:w-[650px] mt-10 self-center transition duration-200 from-gray-200 to-gray-400/30 hover:from-gray-300 hover:to-gray-400/30 border-zinc-400 hover:border-zinc-500 border-2">
-            <img
-            className='mx-auto'
-            src={`${item.image}`}
-            srcSet={`${item.image}`}
-            alt={item.title}
-            loading="lazy"
-            />
-            <div className="border-t-2 border-zinc-400">
-            {item.sizes ? <Sizes item={item} flex={'row'} /> : <AddToCart item={item} /> }
-            </div>
-            <div>
-            <div className="justify-between p-2 flex text-lg font-bold border-t-2 border-zinc-400">
-                <div>{item.title}</div>
-                <div>{item.sale ? <span>{item.sale}€ <span className="line-through decoration-2 decoration-red-600/70">{item.price}€</span></span> : item.price + '€' }</div>
-            </div>
-            <div className="p-2 overflow-clip text-justify">
-                {item.description}
-                <div className="flex justify-end">
-                <Tooltip
-                TransitionComponent={Fade}
-                TransitionProps={{ timeout: 600 }}
-                title="Add an Alarm">
-                    <IconButton className='hover:stroke-black/20' aria-label="Add an Alarm">
-                    <AlarmIcon />
-                    </IconButton>
-                </Tooltip>
+        <div className='flex flex-col overflow-hidden h-full text-gray-700'>
+            {product ?
+            <div className="flex flex-col md:flex-row p-4 sm:p-10 w-[95vw] sm:w-4/5 cursor-default mt-8 self-center transition duration-200 from-gray-200 to-gray-400/30 hover:from-gray-300 hover:to-gray-400/30 border-zinc-400 hover:border-zinc-500 border-2">
+                <div className="mx-auto h-[400px] sm:h-[600px] md:w-1/2 overflow-y-hidden bg-white border-2 border-zinc-400 hover:border-zinc-500 transition duration-200 items-center flex">
+                    <img
+                    className='mx-auto'
+                    src={`${product.image}`}
+                    srcSet={`${product.image}`}
+                    alt={product.title}
+                    loading="lazy"
+                    />
+                </div>
+                <div className="md:pl-10 md:w-1/2 mt-2 md:mt-0">
+                    <div product="border-zinc-400">
+                        {product.sizes ? 
+                        <SizeMenu item={product} /> 
+                        : 
+                        <AddToCart item={product} /> 
+                        }
+                    </div>
+                    <div className="justify-between p-2 flex text-lg font-bold border-t-2 border-zinc-400 md:mt-4 md:pt-4">
+                        <div>{product.title}</div>
+                        <div>
+                            {product.sale ?
+                            <div className="flex gap-x-2">
+                                <span>{product.sale}€</span>
+                                <span className="line-through decoration-2 decoration-red-600/70">
+                                    {product.price}€
+                                </span>
+                            </div>
+                            : 
+                            <span>{product.price}€</span> 
+                            }
+                        </div>
+                    </div>
+                    <div className="p-2 overflow-clip text-justify">
+                        {product.description}
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
-        : <Skeleton variant="rectangular" />}
-        {itemData.length > 0 ?
-        <div className='mx-auto mt-3 px-2 mb-10'>
-            <div className='mt-10 text-lg text-center font-semibold'>RELATED ITEMS
-            <ImageList cols={isMobile ? 2 : 4} gap={2}>
-                {itemData.map((product) => (
-                <Tooltip
-                key={product.id}
-                TransitionComponent={Fade}
-                TransitionProps={{ timeout: 600 }}
-                title={product.title}>
-                    <div
-                    onClick={(e => {
-                    e.preventDefault();
-                    setProduct(product);
-                    navigate(product.title, '/' + product.genre + '/' + product.class + '/' + product.type + '/' + item.id);
-                    })}
-                    >
-                    <div className='h-fit flex relative flex-col cursor-pointer'>
-                        <ImageListItem
-                        className='transition duration-200 bg-gradient-to-br bg-zinc-200 hover:bg-zinc-300 border-zinc-400 hover:border-zinc-500 hover:text-zinc-800 border-2 w-[150px] md:w-[200px]'
-                        key={product.id}>
-                            <img
-                            className='max-h-[200px] md:max-h-[250px]'
-                            src={`${product.image}`}
-                            srcSet={`${product.image}`}
-                            alt={product.title}
-                            loading="lazy"
-                            />
-                        <ImageListItemBar
-                            className='mx-2'
-                            position="below"
-                            title={product.title}
-                            subtitle={product.sale ? <span>{product.sale}€ <span className="line-through decoration-2 decoration-red-600/70">{product.price}€</span></span> : product.price + '€' }
-                        />
-                        </ImageListItem>
-                    </div>
-                    </div>
-                </Tooltip>
-                ))}
-            </ImageList>
-            </div>
-        </div>
-        : undefined }
+            : <Skeleton variant="rectangular" />}
+            <RelatedItems catalog={catalog} item={product} genre={genre} category={category} />
         </div>
     )
 }
