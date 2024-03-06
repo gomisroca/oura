eebe4bcee9f576ac5367eca6ffd0846601ca01e1
asfalfa <glkin9@gmail.com>
2024-03-06 16:38:23 +0100

    style(Client): :lipstick: small styling fix for admin views

--- client/src/pages/Admin/AdminProtectedRoute.jsx
+++ client/src/pages/Admin/AdminProtectedRoute.jsx
@@ -6,11 +6,11 @@ export default function AdminProtectedRoute() {
     console.log(user)
     return (
         user && user.admin ? 
-        <div className="relative flex items-center justify-center">
+        <div className="w-full relative flex items-center justify-center">
             <Outlet/> 
         </div>
         : 
-        <div className="flex flex-col items-center mt-10">
+        <div className="w-full flex flex-col items-center mt-10">
             <span className='uppercase'>You must be logged in and verified to proceed.</span>
             <span>If you believe you should be allowed access, please contact the site admin.</span>
         </div>
