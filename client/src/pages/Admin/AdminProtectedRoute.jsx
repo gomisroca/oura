e61c88afe3f51fa94bf616df7ad02c5e8393bbc0
asfalfa <glkin9@gmail.com>
2024-03-07 12:45:27 +0100

    feat(Client): :sparkles: Added go back button for error and protected routes

--- client/src/pages/Admin/AdminProtectedRoute.jsx
+++ client/src/pages/Admin/AdminProtectedRoute.jsx
@@ -1,7 +1,8 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/UserContext";
 
 export default function AdminProtectedRoute() {
+    const navigate = useNavigate();
     const { user } = useUser();
     console.log(user)
     return (
@@ -13,6 +14,11 @@ export default function AdminProtectedRoute() {
         <div className="w-full flex flex-col items-center mt-10">
             <span className='uppercase'>You must be logged in and verified to proceed.</span>
             <span>If you believe you should be allowed access, please contact the site admin.</span>
+            <div 
+            onClick={() => navigate('/')}
+            className='mt-5 px-5 py-2 uppercase w-full block text-center cursor-pointer border border-zinc-400 hover:bg-zinc-300'>
+                Go Back
+            </div>
         </div>
     )
 };
--- client/src/pages/Error/index.jsx
+++ client/src/pages/Error/index.jsx
@@ -1,16 +1,35 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function Error() {
+    const navigate = useNavigate();
     const error = useRouteError();
-    console.error(error);
 
     return (
-        <div id="error-page" className="text-zinc-700">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+        <div className="flex flex-col items-center text-zinc-700 mt-10">
+            {error ?
+            <div className="flex flex-col items-center">
+                <span>
+                    Sorry, an unexpected error has occurred.
+                </span>
+                <span>
+                    <i>{error.statusText || error.message}</i>
+                </span>
+            </div>
+            : 
+            <div className="flex flex-col items-center gap-4">
+                <div className="text-9xl font-bold">
+                    404
+                </div>
+                <div>
+                    We are not too sure how you landed here.
+                </div>
+            </div>
+            }
+            <div 
+            onClick={() => navigate('/')}
+            className='mt-5 px-5 py-2 uppercase w-full block text-center cursor-pointer border border-zinc-400 hover:bg-zinc-300'>
+                Go Back
+            </div>
         </div>
     );
 }
