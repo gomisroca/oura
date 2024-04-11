5555987e1203978cf0dcac503e9ed0aed1a97531
asfalfa <glkin9@gmail.com>
2024-04-11 13:32:15 +0200

    fix(Client): :bug: maybe even simpler error works?

--- client/app/error/page.tsx
+++ client/app/error/page.tsx
@@ -1,14 +1,9 @@
 'use client' // Error components must be Client Components
  
 import Link from 'next/link'
-import { useEffect } from 'react'
  
-export default function Error({ error }) {
-    useEffect(() => {
-        // Log the error to an error reporting service
-        console.error(error)
-    }, [error])
-    
+export default function Error() {
+
     return (
         <div>
             <h2>Something went wrong!</h2>
