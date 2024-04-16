2cc86962e2ded42b33de1491b3c55af972721998
asfalfa <glkin9@gmail.com>
2024-04-16 20:12:05 +0200

    feat(Client): :sparkles: added wrapper to category menu to avoid needless fetching

--- client/components/navigation/navbar.tsx
+++ client/components/navigation/navbar.tsx
@@ -1,10 +1,10 @@
-'use client'
-
 import UserWrapper from "@/components/navigation/navbar/user-wrapper";
-import CategoryMenu from "@/components/navigation/navbar/category-menu";
+import CategoryMenuWrapper from "@/components/navigation/navbar/category-menu-wrapper"
 import Link from "next/link";
 import { Urbanist } from 'next/font/google'
-import { useEffect, useState } from "react";
+import { getHomepageSettings, getNavigationSettings } from "@/utils/settings";
+import { NavbarSettings } from "@prisma/client";
+import { getCategories } from "@/utils/categories";
  
 const urbanist = Urbanist({
     subsets: ['latin'],
@@ -12,60 +12,10 @@ const urbanist = Urbanist({
     weight: '600'
 })
 
-async function getCategories(){
-    try{
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories/`)
-        if(!res.ok){
-            return null
-        }
-
-        return res.json();
-    } catch(err){
-        console.log(err)
-    }
-}
-
-async function getSettings(){
-    try{
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/settings/navigation`)
-        if(!res.ok){
-            return null
-        }
-
-        return res.json();
-    } catch(err){
-        console.log(err)
-    }
-}
-
-export default function Navbar() {
-    const [filteredCategories, setFilteredCategories] = useState<Category>();
-
-    async function getData() {
-        const categories = await getCategories();
-        const settings = await getSettings();
-        let filteredCategories: Category = {};
-        if(categories && settings){
-            settings?.categories.forEach((category: string) => {
-                const cat = category.toLocaleLowerCase();
-                if (categories && categories[cat]) {
-                    const categoryObject = categories[cat];
-                    if (!filteredCategories[cat]) {
-                        // If the category key doesn't exist in 'filteredCategories', create it as an array
-                        filteredCategories[cat] = categoryObject;
-                    } else {
-                       // If the category key already exists, push the categoryObject into the array
-                        filteredCategories[cat].push(categoryObject);
-                    }
-                }
-            })
-        }
-        setFilteredCategories(filteredCategories)
-    }
-
-    useEffect(() => {
-        getData();
-    }, [])
+export default async function Navbar() {
+    const navigationSettings: NavbarSettings | null  = await getNavigationSettings();
+    const homepageSettings: HomepageSettings | null = await getHomepageSettings();
+    const categories: Category | null = await getCategories();
     
     return (
         <>
@@ -75,12 +25,10 @@ export default function Navbar() {
             href={'/'}>
                 OURA
             </Link>
+            {navigationSettings && homepageSettings && categories &&
             <div className="my-auto flex flex-row items-center">
-                {filteredCategories &&
-                Object.entries(filteredCategories).map(([gender, categories]) => (
-                    <CategoryMenu key={gender} gender={gender} categories={categories} />
-                ))}
-            </div>
+                <CategoryMenuWrapper navigationSettings={navigationSettings} homepageSettings={homepageSettings} categories={categories} />
+            </div>}
             <div className="absolute right-0 self-center">
                 <UserWrapper />
             </div>
---
+++ client/components/navigation/navbar/category-menu-wrapper.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { HomepageSettings, NavbarSettings } from "@prisma/client"
+import CategoryMenu from "@/components/navigation/navbar/category-menu"
+import { useEffect, useState } from "react"
+
+interface Props {
+    navigationSettings: NavbarSettings,
+    homepageSettings: HomepageSettings,
+    categories: any
+}
+  
+export default function CategoryMenuWrapper({ navigationSettings, homepageSettings, categories }: Props) {
+    const [filteredCategories, setFilteredCategories] = useState<Category>();
+
+    function filterCategories(){
+        let filteredCategories: Category = {};
+        if(categories && navigationSettings){
+            navigationSettings?.categories.forEach((category: string) => {
+                const cat = category.toLocaleLowerCase();
+                if (categories && categories[cat]) {
+                    const categoryObject = categories[cat];
+                    if (!filteredCategories[cat]) {
+                        // If the category key doesn't exist in 'filteredCategories', create it as an array
+                        filteredCategories[cat] = categoryObject;
+                    } else {
+                    // If the category key already exists, push the categoryObject into the array
+                        filteredCategories[cat].push(categoryObject);
+                    }
+                }
+            })
+        }
+        setFilteredCategories(filteredCategories)
+    }
+
+    useEffect(() => {
+        filterCategories()
+    },[])
+
+    return (
+        <>
+            {filteredCategories &&
+            Object.entries(filteredCategories).map(([gender, categories]) => (
+                <CategoryMenu homepageSettings={homepageSettings} key={gender} gender={gender} categories={categories} />
+            ))}
+        </>
+    )
+}
