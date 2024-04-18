cae2a4d7fb20d094274553af3bc975c65712320e
asfalfa <glkin9@gmail.com>
2024-04-18 17:21:06 +0200

    vercel blob 2

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
     distDir: './dist', // Changes the build output directory to `./dist/`.  
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'oura-blob.public.blob.vercel-storage.com',
+            port: '',
+          },
+        ],
+    },
 }
    
 export default nextConfig
