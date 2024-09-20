c520824ba06b13f9057484245b4aef70fa8f3a88
asfalfa <glkin9@gmail.com>
2024-09-20 16:53:16 +0200

    feat(components): add ph for missing image

--- src/app/_components/product/ProductCard.tsx
+++ src/app/_components/product/ProductCard.tsx
@@ -21,18 +21,20 @@ function ProductCard({ product }: { product: Product }) {
     <div
       onClick={() => setShowDetails(!showDetails)}
       key={product.id}
-      className="group relative flex h-[45rem] w-[30rem] flex-col items-center justify-center gap-2 overflow-hidden rounded-xl border border-slate-600/10 bg-slate-200/30 shadow-md transition duration-200 ease-in-out hover:border-slate-600/20 hover:bg-slate-300/30 dark:border-slate-400/10 dark:bg-slate-800/30 hover:dark:border-slate-400/20 dark:hover:bg-slate-700/30">
-      {product.image && (
-        <Image
-          className="h-full w-full cursor-pointer rounded-t-xl object-cover duration-200 ease-in-out group-hover:contrast-[1.05]"
-          src={`https://${env.NEXT_PUBLIC_IMAGE_PROXY_HOSTNAME}/storage/v1/object/public/${product.image}`}
-          alt={product.name}
-          width={400}
-          height={400}
-        />
-      )}
+      className="group relative flex h-[30rem] w-[30rem] flex-col items-center justify-center gap-2 overflow-hidden rounded-xl border border-slate-600/10 bg-slate-200/30 shadow-md transition duration-500 ease-in-out hover:border-slate-600/40 hover:bg-slate-300/30 dark:border-slate-400/10 dark:bg-slate-800/30 dark:shadow-slate-500/10 hover:dark:border-slate-400/40 dark:hover:bg-slate-700/30">
+      <Image
+        className="h-full w-full cursor-pointer rounded-t-xl object-cover duration-200 ease-in-out group-hover:contrast-[1.05]"
+        src={
+          product.image
+            ? `https://${env.NEXT_PUBLIC_IMAGE_PROXY_HOSTNAME}/storage/v1/object/public/${product.image}`
+            : '/ph_item.png'
+        }
+        alt={product.name}
+        width={400}
+        height={400}
+      />
       <div
-        className={`absolute bottom-[-10rem] z-10 flex w-full flex-col items-center justify-center gap-2 bg-slate-800 p-4 opacity-0 duration-500 ease-in-out group-hover:translate-y-[-10rem] group-hover:opacity-100 ${showDetails ? 'translate-y-[-10rem] opacity-100' : 'opacity-0'}`}>
+        className={`absolute bottom-[-10rem] z-10 flex w-full flex-col items-center justify-center gap-2 bg-slate-200 p-4 opacity-0 duration-500 ease-in-out group-hover:translate-y-[-10rem] group-hover:opacity-100 dark:bg-slate-800 ${showDetails ? 'translate-y-[-10rem] opacity-100' : 'opacity-0'}`}>
         <h2>{product.name}</h2>
         <p>{product.description}</p>
         <p>Base Price: {product.basePrice}</p>
