cebdcc9317e4e7ebe8894545008b3cea16b988d9
asfalfa <glkin9@gmail.com>
2024-09-20 12:29:08 +0200

    style(components): minor styling changes

--- src/app/_components/ui/Button.tsx
+++ src/app/_components/ui/Button.tsx
@@ -27,7 +27,7 @@ function Button({ name, type = 'button', disabled = false, onClick, className, c
     name={name || 'Button'}
     type={type} 
     onClick={onClick} 
-    className={`${className} border dark:border-slate-400/10 border-slate-600/10 rounded-full bg-slate-200/30 dark:bg-slate-800/30 dark:hover:bg-slate-700/30 px-10 py-3 shadow-md font-semibold transition hover:bg-slate-300/30 ${disabled ? 'cursor-not-allowed' : ''} flex flex-row gap-2 items-center`} disabled={disabled}>
+    className={`${className} border dark:border-slate-400/10 duration-200 border-slate-600/10 rounded-full bg-slate-200/30 dark:bg-slate-800/30 dark:hover:bg-slate-700/30 px-10 py-3 shadow-md font-semibold transition hover:bg-slate-300/30 ${disabled ? 'cursor-not-allowed' : ''} flex flex-row gap-2 items-center`} disabled={disabled}>
       {children}
     </button>	
   )
