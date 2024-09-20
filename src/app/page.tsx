58372e8470553016933e8e4a29f76bf466896e1d
asfalfa <glkin9@gmail.com>
2024-09-20 21:34:52 +0200

    feat(client): added product list and form temporarily to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { getServerAuthSession } from '@/server/auth';
+import ProductForm from './_components/product/ProductForm';
+import ProductList from './_components/product/ProductList';
 
 export default async function Home() {
   const session = await getServerAuthSession();
-  return session ? <p>You are signed in as {session.user.name}</p> : <p>You are not signed in</p>;
+  return (
+    <div className="flex w-full flex-col gap-2">
+      {session ? <p>You are signed in as {session.user.name}</p> : <p>You are not signed in</p>}
+      <ProductForm />
+      <ProductList />
+    </div>
+  );
 }
