bf88e5ae108382e82fda003ba026f624b201c350
asfalfa <glkin9@gmail.com>
2024-09-28 16:09:13 +0200

    fix(components): check for session before displaying cart foldable

--- src/app/_components/navbar/CartFoldable.tsx
+++ src/app/_components/navbar/CartFoldable.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import Foldable from '../ui/Foldable';
 import { FaCartShopping } from 'react-icons/fa6';
 import CartList from '../cart/CartList';
+import { getServerAuthSession } from '@/server/auth';
 
 async function CartFoldable() {
+  const session = await getServerAuthSession();
+  if (!session) return;
   return (
     <Foldable
       button={{ name: 'Cart', text: <FaCartShopping size={20} />, className: 'px-[0.75rem] xl:px-10' }}
