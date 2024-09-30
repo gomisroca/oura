5962753d1763633176ae7b59846c28c804510107
asfalfa <glkin9@gmail.com>
2024-09-30 11:06:59 +0200

    fix(components): remove mock add to cart

--- src/app/checkout/CheckoutContent.tsx
+++ src/app/checkout/CheckoutContent.tsx
@@ -1,12 +1,9 @@
 import CheckoutButton from './CheckoutButton';
-import MockAddToCart from './MockAddToCart';
 import CartList from '../_components/cart/CartList';
 
 function CheckoutContent() {
   return (
     <div className="flex flex-col items-center justify-center gap-6">
-      {/* Mock Adding Products to the Cart */}
-      <MockAddToCart />
       {/* Cart Summary */}
       <CartList />
       {/* Checkout Button where the checkout logic resides */}
