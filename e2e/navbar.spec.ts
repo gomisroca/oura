f56a01671998d0e35d791d2ce4447ce3368b6502
asfalfa <glkin9@gmail.com>
2024-10-02 12:04:52 +0200

    fix(e2e): adjust navbar e2e test 2

--- e2e/navbar.spec.ts
+++ e2e/navbar.spec.ts
@@ -4,11 +4,11 @@ test('has initial buttons', async ({ page }) => {
   await page.goto('/');
 
   await expect(page.getByRole('button', { name: 'Logo' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Sports' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Categories' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'General Menu' })).toBeVisible();
 });
 
-test('sport foldable is visible', async ({ page }) => {
+test('categories foldable is visible', async ({ page }) => {
   await page.goto('/');
 
   await expect(page.getByRole('button', { name: 'Categories' })).toBeVisible();
