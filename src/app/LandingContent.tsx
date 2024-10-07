1f5c0db8463881b5d24856374e899485528982f3
asfalfa <glkin9@gmail.com>
2024-10-07 12:49:45 +0200

    feat(client): add defaults for landing page

--- src/app/LandingContent.tsx
+++ src/app/LandingContent.tsx
@@ -5,23 +5,31 @@ import { type ProductWithSizes } from 'types';
 import Button from './_components/ui/Button';
 import Carousel from './_components/ui/Carousel';
 
-export default function LandingContent({ sale, products }: { sale: Sale; products: ProductWithSizes[] }) {
+export default function LandingContent({ sale, products }: { sale?: Sale; products: ProductWithSizes[] }) {
   return (
     <div className="absolute left-0 right-0 top-0 flex min-h-screen flex-col items-center justify-evenly overflow-hidden">
       {/* Sale Title */}
       <div className="flex flex-col items-center">
         <h1 className="text-center text-5xl font-bold drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(255_255_255_/_40%)] dark:[text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)] md:text-7xl">
-          {sale.name.toUpperCase()}
+          {sale ? sale.name.toUpperCase() : 'OURA'}
         </h1>
-        <h6 className="text-center font-bold drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(255_255_255_/_40%)] dark:[text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)]">
-          {sale.startDate.toLocaleDateString()} - {sale.endDate.toLocaleDateString()}
-        </h6>
+        <p className="text-center text-sm font-bold uppercase drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(255_255_255_/_40%)] dark:[text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)]">
+          {sale
+            ? `${sale.startDate.toLocaleDateString()} - ${sale.endDate.toLocaleDateString()}`
+            : 'Sports with a purpose'}
+        </p>
       </div>
       {products && (
         <div className="flex flex-col items-center justify-center gap-2">
-          <Button onClick={() => alert('Browse Sale')} className="w-[80vw] md:w-[25vw] 2xl:w-[10vw]">
-            Browse Sale
-          </Button>
+          {sale ? (
+            <Button onClick={() => alert('Browse Sale')} className="w-[80vw] md:w-[25vw] 2xl:w-[10vw]">
+              Browse Sale
+            </Button>
+          ) : (
+            <Button onClick={() => alert('Browse Products')} className="w-[80vw] md:w-[25vw] 2xl:w-[10vw]">
+              Browse Products
+            </Button>
+          )}
           <Carousel products={products} />
         </div>
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,38 @@
 import Image from 'next/image';
 import { env } from '@/env';
 import { api } from '@/trpc/server';
-import MessageWrapper from './_components/ui/MessageWrapper';
 import LandingContent from './LandingContent';
 import { type ProductWithSizes } from 'types';
 
 async function LandingHero() {
+  // Get ongoing sale info
   const sale = await api.sale.get();
-  const products: ProductWithSizes[] | undefined = sale?.products.sort(
-    (a, b) => b.amountSold + b.views - (a.amountSold + a.views)
-  );
 
-  // If there is no sale, it should go back to some defaults, a default image, default sale name 'Hottest Deals' or whatever, and the most populart products (sales+views)
-  if (!sale) return <MessageWrapper message="Unable to fetch sale at this time" />;
+  // Get products in the sale, if there is one, otherwise get all products
+  let products: ProductWithSizes[] | undefined;
+  if (sale) {
+    products = sale?.products;
+  } else {
+    products = await api.product.getAll();
+  }
+  // Sort products by popularity (views + sales)
+  products = products.sort((a, b) => b.amountSold + b.views - (a.amountSold + a.views));
+
   return (
     <>
-      {/* Sale Image */}
+      {/* Landing Image */}
       <Image
-        src={`https://${env.NEXT_PUBLIC_IMAGE_PROXY_HOSTNAME}/storage/v1/object/public/${sale.image}`}
+        src={
+          sale
+            ? `https://${env.NEXT_PUBLIC_IMAGE_PROXY_HOSTNAME}/storage/v1/object/public/${sale.image}`
+            : '/landing.jpg'
+        }
         alt="sale"
         width={1920}
         height={1080}
-        className="pointer-events-none absolute bottom-0 left-0 right-0 top-0 min-h-screen w-full object-cover object-center xl:h-full"
+        className={`pointer-events-none absolute bottom-0 left-0 right-0 top-0 min-h-screen w-full object-cover object-center xl:h-full ${!sale ? 'opacity-50 grayscale dark:brightness-50' : ''}`}
       />
-      {products && <LandingContent sale={sale} products={products} />}
+      {products && <LandingContent sale={sale ?? undefined} products={products} />}
     </>
   );
 }
