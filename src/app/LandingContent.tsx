aecbb7f6eebb23a2b56fe3b1e7b37f25f9aa7f8d
asfalfa <glkin9@gmail.com>
2024-10-08 17:51:37 +0200

    feat(style): adjust landing page title

--- src/app/LandingContent.tsx
+++ src/app/LandingContent.tsx
@@ -19,10 +19,15 @@ export default function LandingContent({ sale, products }: { sale?: Sale; produc
     <div className="absolute left-0 right-0 top-0 flex min-h-screen flex-col items-center justify-evenly overflow-hidden">
       {/* Sale Title */}
       <div className="flex flex-col items-center">
-        <h1 className="text-center text-5xl font-bold drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(255_255_255_/_40%)] dark:[text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)] md:text-7xl">
+        {sale && (
+          <h4 className="text-center text-2xl font-bold text-slate-200 drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)] md:text-4xl">
+            OURA
+          </h4>
+        )}
+        <h1 className="text-center text-5xl font-bold text-slate-200 drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)] md:text-7xl">
           {sale ? sale.name.toUpperCase() : 'OURA'}
         </h1>
-        <p className="text-center text-sm font-bold uppercase drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(255_255_255_/_40%)] dark:[text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)]">
+        <p className="text-center font-bold uppercase text-slate-200 drop-shadow-lg [text-shadow:_2px_2px_4px_rgb(0_0_0_/_40%)]">
           {sale
             ? `${sale.startDate.toLocaleDateString()} - ${sale.endDate.toLocaleDateString()}`
             : 'Sports with a purpose'}
