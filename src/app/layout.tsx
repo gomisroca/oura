83432b9e394c167aeb8f50c0d553ade798d4071c
asfalfa <glkin9@gmail.com>
2024-10-11 09:47:15 +0200

    fix(components): streamline landing page logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,21 @@ import { type Metadata } from 'next';
 
 import { TRPCReactProvider } from '@/trpc/react';
 import { ThemeProvider } from 'next-themes';
-import Navbar from './_components/navbar';
 import { MessageProvider } from '@/context/MessageContext';
+import Navbar from './_components/navbar';
 import Message from './_components/ui/Message';
 
 export const metadata: Metadata = {
   title: 'OURA Clothing',
   description: 'Eco-friendly sports clothing',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 const worksans = Work_Sans({
   subsets: ['latin'],
+  weight: ['400', '600'], // Add specific weights if needed
 });
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
@@ -30,10 +33,13 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                 style={{ backgroundImage: "url('/bg.jpg')" }}
                 className="min-h-screen bg-cover bg-fixed bg-center bg-no-repeat">
                 <Navbar />
-                <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-slate-50 via-slate-200/90 to-slate-800/40 px-4 pb-10 pt-20 text-slate-800 dark:from-slate-950 dark:via-slate-900/95 dark:to-slate-900/60 dark:text-slate-200 xl:px-32">
+                <main
+                  role="main"
+                  className="flex min-h-screen items-center justify-center bg-gradient-to-br from-slate-50 via-slate-200/90 to-slate-800/40 px-4 pb-10 pt-20 text-slate-800 dark:from-slate-950 dark:via-slate-900/95 dark:to-slate-900/60 dark:text-slate-200 xl:px-32">
                   {children}
-                  <Message />
                 </main>
+                {/* Message Component placed outside of main for better structure */}
+                <Message />
               </div>
             </TRPCReactProvider>
           </MessageProvider>
