f34025905c5ad81837f99d17b0389937860d3eb5
asfalfa <glkin9@gmail.com>
2024-10-14 14:33:17 +0200

    style(admin): adjust styling of admin panel buttons

--- src/app/admin/AdminPanel.tsx
+++ src/app/admin/AdminPanel.tsx
@@ -7,22 +7,48 @@ import { useRouter } from 'next/navigation';
 function AdminPanel() {
   const router = useRouter();
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex flex-row gap-2">
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col items-center justify-center gap-2">
         <h1 className="text-xl uppercase">Products</h1>
-        <Button onClick={() => router.push('/admin/create/product')} name="Create Product">
-          Create Product
-        </Button>
-        <Button onClick={() => router.push('/admin/update/product')} name="Update Product">
-          Update Product
-        </Button>
+        <div className="flex flex-row gap-2">
+          <Button onClick={() => router.push('/admin/create/product')} name="Create Product">
+            Create Product
+          </Button>
+          <Button onClick={() => router.push('/admin/update/product')} name="Update Product">
+            Update Product
+          </Button>
+        </div>
       </div>
-      <Button onClick={() => router.push('/admin/create/category')} name="Create Category">
-        Create Category
-      </Button>
-      <Button onClick={() => router.push('/admin/create/sale')} name="Create Sale">
-        Create Sale
-      </Button>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1 className="text-xl uppercase">Categories</h1>
+        <div className="flex flex-row gap-2">
+          <Button onClick={() => router.push('/admin/create/category')} name="Create Category">
+            Create Category
+          </Button>
+          <Button onClick={() => router.push('/admin/update/category')} name="Update Category">
+            Update Category
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1 className="text-xl uppercase">Sales</h1>
+        <div className="flex flex-row gap-2">
+          <Button onClick={() => router.push('/admin/create/sale')} name="Create Sale">
+            Create Sale
+          </Button>
+          <Button onClick={() => router.push('/admin/update/sale')} name="Update Sale">
+            Update Sale
+          </Button>
+        </div>
+      </div>
+      {/* 
+      There should be update options for each of these.
+      When updating a product, it should be possible to update the sizes, colors, stock.
+          It might be interesting to add 'tags' to the product model, to get more accurate related products.
+      When updating a sport, category or subcategory, it should be possible to delete it or its categories/subcategories (with a warning of how many products will be left orphaned). 
+          It should also be possible to update which products belong to it.
+      When updating a sale, it should be possible to update the date, time, and which products belong to it.
+      */}
     </div>
   );
 }
