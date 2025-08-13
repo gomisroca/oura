8e65318ef659aa0d42d0503f610a1bb0a8398bbb
Adrià Gomis <gomisroca@gmail.com>
2025-08-13 09:07:46 +0200

    fix: change place of sportslist component

--- src/app/sport/page.tsx
+++ src/app/sport/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation';
 
 import { api } from '@/trpc/server';
 
-import SportList from '../_components/ui/SportList';
+import SportList from './SportList';
 
 export default async function SportsList({ searchParams }: { searchParams: Promise<{ gender: 'man' | 'woman' }> }) {
   const searchParamsData = await searchParams;
@@ -12,9 +12,7 @@ export default async function SportsList({ searchParams }: { searchParams: Promi
   try {
     const [products, sports] = await Promise.all([api.product.getAll(gender), api.category.getSports()]);
 
-    if (products.length === 0) {
-      notFound();
-    }
+    if (products.length === 0) return <p>No products found in this category.</p>;
 
     return <SportList products={products} sports={sports} />;
   } catch (_error) {
