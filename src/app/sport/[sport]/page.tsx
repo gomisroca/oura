885c7ba024a021ab90fa849106a7dd89d3f48026
Adrià Gomis <gomisroca@gmail.com>
2025-08-14 09:38:47 +0200

    feat: show categories in sport view

--- src/app/sport/[sport]/page.tsx
+++ src/app/sport/[sport]/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 import ProductList from '@/app/_components/product/ProductList';
@@ -18,8 +19,9 @@ export default async function SportList({
 
   try {
     const products = await api.product.getBySport({ sportId: Number(paramsData.sport), gender: gender });
+    const subcategories = await api.category.getCategories({ sportId: Number(paramsData.sport) });
 
-    if (products.length === 0) notFound();
+    if (products.length === 0) return <p>No products found in this category.</p>;
     return (
       <div className="flex h-full flex-col justify-start gap-4">
         <div className="flex flex-row items-center justify-center md:absolute md:top-24 md:right-0 md:left-0">
@@ -28,6 +30,17 @@ export default async function SportList({
             <span className="text-neutral-800 dark:text-neutral-400">{products[0]?.sport?.name}</span>
           </div>
         </div>
+        {subcategories && subcategories.length > 0 && (
+          <div className="flex flex-row items-center justify-center gap-x-4 md:absolute md:top-34 md:right-0 md:left-0">
+            {subcategories?.map((subcategory) => (
+              <Link href={`/sport/${paramsData.sport}/${subcategory.id}`} key={subcategory.id}>
+                <h2 className="cursor-pointer text-sm uppercase transition-colors duration-200 hover:text-neutral-700 dark:hover:text-neutral-300">
+                  {subcategory.name}
+                </h2>
+              </Link>
+            ))}
+          </div>
+        )}
         <ProductList products={products} />
       </div>
     );
