4752f72514f46a38eb9558b475ab9a36ec970d39
Adrià Gomis <gomisroca@gmail.com>
2026-01-05 22:41:32 +0100

    feat: adjust message txt and style

--- src/app/_components/ui/Message.tsx
+++ src/app/_components/ui/Message.tsx
@@ -33,14 +33,12 @@ const Message = () => {
 
   return (
     <div
-      className={`fixed m-auto left-0 right-0 z-[99] w-[90vw] xl:w-[30vw] flex flex-col items-center justify-center gap-2 px-5 py-2 font-semibold border rounded-sm ${
+      className={`fixed m-auto left-0 right-0 border-4 z-[99] w-[90vw] xl:w-[30vw] flex flex-col items-center justify-center gap-2 px-5 py-2 font-semibold rounded-sm bg-white/95 dark:bg-black/95 ${
         error
-          ? 'border-red-500 bg-red-200/90 dark:bg-red-800/90'
-          : 'border-green-500 bg-green-200/90 dark:bg-green-800/90'
+          ? 'border-red-500'
+          : 'border-green-500'
       } ${popup ? 'bottom-10' : 'bottom-auto'}`}
     >
-      {/* Conditional Text Based on Error */}
-      <p>{error ? "An error occurred" : "Success"}</p>
       <h1 className="text-xl">{message}</h1>
       {error && <p>Please try again</p>}
     </div>
